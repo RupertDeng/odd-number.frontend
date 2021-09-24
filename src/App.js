e64f966a405fa95c5f5a1c802973c040a8b12a9e
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-09-24 11:06:07 -0400

    clean up initial react app

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Website currently under construction. <br></br>
+          Please visit later.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
