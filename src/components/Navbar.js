d14269f7fe84366b7283ff5eb13c7eca7ff13448
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-10-09 11:53:53 -0400

    change nav toggler icon style and response

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import './Navbar.css'
 
 export const Navbar = () => {
 
@@ -7,15 +7,16 @@ export const Navbar = () => {
     <nav className='navbar navbar-expand-sm navbar-dark fixed-top'>
       <div className='container-md pt-1 pb-1 ps-3 pe-3'>
         <a class='navbar-brand' href='/'>
-          <img src='/images/logo-large.png' alt='' height='32'></img>
+          <img src='/images/logo-large.png' alt='' height='30'></img>
         </a>
-        <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#collapsibleNavbar' aria-controls='collapsibleNavbar' aria-expanded='false' aria-label='Toggle navigation'>
-          <span class='navbar-toggler-icon'></span>
+        <button class='navbar-toggler collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#collapsibleNavbar' aria-controls='collapsibleNavbar' aria-expanded='false' aria-label='Toggle navigation'>
+          <span class='toggler-icon top-bar'></span>
+          <span class='toggler-icon bottom-bar'></span>
         </button>
         <div class='collapse navbar-collapse' id='collapsibleNavbar'>
           <div class='navbar-nav ms-auto'>
             <a class='nav-link me-3' href='/'>Terms of Service</a>
-            <a class='nav-link' href='/'>Privacy Policy</a>
+            <a class='nav-link me-3' href='/'>Privacy Policy</a>
           </div>
         </div>
       </div>
