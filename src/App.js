44ea46aa96d76a8bb561173191a2c818bd5a32ad
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-10-08 23:55:13 -0400

    added collapse navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import {Navbar} from './components/Navbar'
 
 function App() {
   return (
-    <div className="app container-fluid">
-      Test Page
-    </div>
+    <>
+      <Navbar />
+
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,26 @@
 import React from 'react';
-import './Navbar.css'
 
+export const Navbar = () => {
+
+
+  return (
+    <nav className='navbar navbar-expand-sm navbar-dark fixed-top'>
+      <div className='container-md pt-1 pb-1 ps-3 pe-3'>
+        <a class='navbar-brand' href='/'>
+          <img src='/images/logo-large.png' alt='' height='32'></img>
+        </a>
+        <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#collapsibleNavbar' aria-controls='collapsibleNavbar' aria-expanded='false' aria-label='Toggle navigation'>
+          <span class='navbar-toggler-icon'></span>
+        </button>
+        <div class='collapse navbar-collapse' id='collapsibleNavbar'>
+          <div class='navbar-nav ms-auto'>
+            <a class='nav-link me-3' href='/'>Terms of Service</a>
+            <a class='nav-link' href='/'>Privacy Policy</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+
+
+}
