189c1cfefcd8b751013a93dc6a3eccca3c5b64f8
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-10-10 03:11:07 -0400

    fix some typos and complete jumbotron

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -2,12 +2,11 @@ import './Jumbo.css';
 
 export const Jumbo = () => {
   return (
-    <div className='container bg-success py-sm-5 py-4' id='jumbo'>
+    <div className='container py-sm-5 py-4' id='jumbo' style={{backgroundImage: 'url(/images/jumbo-background.png'}}>
       <div className='w-75 m-auto'>
-        <p className='display-6 text-white-50'>Called by un-familiar phone numbers? Or your own number is being exploited in spoofing?</p>
+        <p className='display-6' id='jumbo-headline'>Called by un-familiar phone numbers? Or your own number is being exploited in spoofing?</p>
         <br></br>
-        <p className='h3 text-white'>You can try search here or share info to help others ..</p>
-        <p className='text-white'><small>Read the terms and policy for more details</small></p>
+        <p className='h3 text-white'>You can try search here or share info to help others alike ..</p>
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,21 +4,21 @@ export const Navbar = () => {
   return (
     <nav className='navbar navbar-expand-sm navbar-dark'>
       <div className='container-lg pt-1 pb-1 ps-4 pe-4'>
-        <a class='navbar-brand' href='/'>
+        <a className='navbar-brand' href='/'>
           <picture>
-            <source media='(max-width: 720px)' srcset='/images/logo-small.png'></source>
-            <source media='(min-width: 721px)' srcset='/images/logo-large.png'></source>
+            <source media='(max-width: 720px)' srcSet='/images/logo-small.png'></source>
+            <source media='(min-width: 721px)' srcSet='/images/logo-large.png'></source>
             <img src='/images/logo-large.png' alt='Odd Number the App' height='28'></img>
           </picture>
         </a>
-        <button class='navbar-toggler collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#collapsibleNavbar' aria-controls='collapsibleNavbar' aria-expanded='false' aria-label='Toggle navigation'>
-          <span class='toggler-icon top-bar'></span>
-          <span class='toggler-icon bottom-bar'></span>
+        <button className='navbar-toggler collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#collapsibleNavbar' aria-controls='collapsibleNavbar' aria-expanded='false' aria-label='Toggle navigation'>
+          <span className='toggler-icon top-bar'></span>
+          <span className='toggler-icon bottom-bar'></span>
         </button>
-        <div class='collapse navbar-collapse' id='collapsibleNavbar'>
-          <div class='navbar-nav ms-auto'>
-            <a class='nav-link me-3' href='/terms-of-service'>Terms of Service</a>
-            <a class='nav-link me-3' href='/privacy-policy'>Privacy Policy</a>
+        <div className='collapse navbar-collapse' id='collapsibleNavbar'>
+          <div className='navbar-nav ms-auto'>
+            <a className='nav-link me-3' href='/terms-of-service'>Terms of Service</a>
+            <a className='nav-link me-3' href='/privacy-policy'>Privacy Policy</a>
           </div>
         </div>
       </div>
