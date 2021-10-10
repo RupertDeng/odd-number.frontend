66170943aac7b725408e1300b40c13061ecf0122
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-10-09 20:07:45 -0400

    add alternative logo at small screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import './Navbar.css'
 
 export const Navbar = () => {
-
-
+  
   return (
     <nav className='navbar navbar-expand-sm navbar-dark fixed-top'>
-      <div className='container-md pt-1 pb-1 ps-3 pe-3'>
+      <div className='container-md pt-1 pb-1 ps-4 pe-4'>
         <a class='navbar-brand' href='/'>
-          <img src='/images/logo-large.png' alt='' height='30'></img>
+          <picture>
+            <source media='(max-width: 350px)' srcset='/images/logo-small.png'></source>
+            <source media='(min-width: 351px)' srcset='/images/logo-large.png'></source>
+            <img src='/images/logo-large.png' alt='Odd Number the App' height='30'></img>
+          </picture>
         </a>
         <button class='navbar-toggler collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#collapsibleNavbar' aria-controls='collapsibleNavbar' aria-expanded='false' aria-label='Toggle navigation'>
           <span class='toggler-icon top-bar'></span>
