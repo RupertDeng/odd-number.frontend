46993de74283d85ea27c7edbefce9caf8611aa4c
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-10-12 23:36:56 -0400

    move bootstrap import under index.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import {Privacy} from './pages/Privacy';
 import {Navbar} from './components/Navbar';
 import {Footer} from './components/Footer';
 import smoothscroll from 'smoothscroll-polyfill';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
 
 
 function App() {
