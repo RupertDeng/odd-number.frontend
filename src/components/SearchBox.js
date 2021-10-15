d8250500a31599d05f8d74d599e584543905b2b5
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-10-15 17:25:49 -0400

    correct cors format

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -35,7 +35,7 @@ export const SearchBox = ({updateSearchResult}) => {
   const searchNumber = async (num) => {
     const result = await axios({
       method: 'get',
-      url: `${process.env.REACT_APP_API_URL}/search/${num}`,
+      url: `${process.env.REACT_APP_API_URL}search/${num}`,
       headers: {'X-Api-Key': process.env.REACT_APP_API_KEY}
     });
     updateSearchResult(result.data);
