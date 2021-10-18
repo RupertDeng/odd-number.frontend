aa3187c7c37c4ccd73daf0d08a653d27c727f7cb
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-10-17 21:59:19 -0400

    styling message poster

--- src/components/MessagePoster.js
+++ src/components/MessagePoster.js
@@ -13,27 +13,27 @@ export const MessagePoster = ({searchResult, setSearchResult}) => {
       posterButton.classList.remove('btn-warning');
       posterButton.classList.add('btn-success');
       posterButton.innerText = 'Post';
-      posterTitle.innerText = ' your own message on this number';
+      posterTitle.innerText = ' your own message';
       clearPoster();
     } else {
       posterButton.classList.remove('btn-success')
       posterButton.classList.add('btn-warning');
       posterButton.innerText = 'Cancel';
-      posterTitle.innerText = ' posting message on this number';
+      posterTitle.innerText = ' posting message';
     }
   }
 
   return (
     <div className='container py-3 border-bottom'>
-      <div className='row'>
-        <div className='col-12 text-center p-0 m-0'>
-          <button type='button' id='poster-button' className='btn btn-success rounded-pill' data-bs-toggle='collapse' data-bs-target='#poster-panel' style={{border: '2px solid rgba(255, 255, 255, 0.8)'}} onClick={handlePosterButton}>Post</button>
-          <span id='poster-title' className='text-light'> your own message on this number</span>
-        </div>
-        <div className='col-12 collapse' id='poster-panel'>
-          <div className='row'>
-            <div className='col-12 col-md-6'>
-              <label>Select one tag:</label>
+      <div className='text-center'>
+        <button type='button' id='poster-button' className='btn btn-success rounded-pill' data-bs-toggle='collapse' data-bs-target='#poster-panel' style={{border: '2px solid rgba(255, 255, 255, 0.8)'}} onClick={handlePosterButton}>Post</button>
+        <span id='poster-title' className='text-light'> your own message</span>
+      </div>
+      <div className='mx-auto mt-3 px-2' style={{maxWidth: '800px'}}>
+        <div className='row rounded bg-light px-2 px-sm-3 gx-1 align-items-center justify-content-around collapse' id='poster-panel'>
+          <div className='col-12 col-md-4 p-2'>
+            <label className='py-2 fw-light fs-5'>Select one tag:</label>
+            <div className='ps-2 border bg-white rounded'>
               <div className='form-check'>
                 <input className='form-check-input' type='radio' name='radio-tag' id='tag-scam'></input>
                 <label className='form-check-label' htmlFor='tag-scam'>scam/spoofing</label>
@@ -51,15 +51,20 @@ export const MessagePoster = ({searchResult, setSearchResult}) => {
                 <label className='form-check-label' htmlFor='tag-normal'>normal business</label>
               </div>
             </div>
-            <div className='col-12 col-md-6'>
-              <label>Enter your message:</label>
-              <textarea className='form-control h-75' id='poster-form' placeholder='Your message will help other people to better identify unknown phone call.'></textarea>
-            </div> 
-            <div className='col-12'>
-              <span className='fst-italic text-danger'><small>Please select one tag and enter message before submitting. </small></span>
-              <button className='btn btn-dark'>Submit</button>
+          </div>
+          <div className='col-12 col-md-7 p-2'>
+            <label className='py-2 fw-light fs-5'>Enter your message:</label>
+            <textarea className='form-control' style={{height: '106px'}} id='poster-form' placeholder='Your message will help other people better identify this number.'></textarea>
+          </div>
+          <div className='col-12 px-4 pt-2 pb-3'>
+            <div className='row align-items-center justify-content-end'>
+              <div className='col-12 col-md-9 text-end d-none'>
+                <span className='fst-italic text-danger'><small>Please select one tag and enter message before submitting. </small></span>
+              </div>
+              <div className='col-12 col-md-2 text-end'>
+                <button className='btn btn-dark'>Submit</button>
+              </div>
             </div>
-            <div></div>
           </div>
         </div>
       </div>
