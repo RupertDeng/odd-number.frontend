cbd70b64776889aa6ba6e876cd38dab3453a7920
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2021-10-21 17:58:07 -0400

    change pagination style

--- src/components/MessageView.js
+++ src/components/MessageView.js
@@ -8,8 +8,9 @@ export const MessageView = ({vidHash, searchResult, handleMessageDelete, handleM
   const [currPage, setCurrPage] = useState(1)  // current page state 
   const msgPerPage = 20;
 
-  console.log(vidHash);
+  console.log(currPage);
 
+  // when user conducts a new search, reset current page to 1. CurrPage will not change when post/delete message or vote message.
   useEffect(() => {
     setCurrPage(1);
   }, [searchResult.number, searchResult.searched])
@@ -26,15 +27,15 @@ export const MessageView = ({vidHash, searchResult, handleMessageDelete, handleM
       const pages = Math.ceil(total / msgPerPage);
       const pagination = [];
       for (let i = 1; i <= pages; i++) {
-        pagination.push((<li key={`page-${i}`} className='page-item'><button className='page-link' onClick={()=>setCurrPage(i)}>{i}</button></li>));
+        pagination.push((<li key={`page-${i}`} className='page-item'><span className={i === currPage ? 'page-link bg-info text-white' : 'page-link'} onClick={()=>setCurrPage(i)}>{i}</span></li>));
       }
 
       return (
         <nav>
           <ul className='pagination justify-content-center'>
-            <li className='page-item'><button className='page-link' onClick={()=>setCurrPage(Math.max(1, currPage-1))}><i className='bi bi-chevron-left'></i></button></li>
+            <li className='page-item'><span className='page-link' onClick={()=>setCurrPage(Math.max(1, currPage-1))}><i className='bi bi-chevron-left'></i></span></li>
             {pagination}
-            <li className='page-item'><button className='page-link' onClick={()=>setCurrPage(Math.min(pages, currPage+1))}><i className='bi bi-chevron-right'></i></button></li>
+            <li className='page-item'><span className='page-link' onClick={()=>setCurrPage(Math.min(pages, currPage+1))}><i className='bi bi-chevron-right'></i></span></li>
           </ul>
         </nav>
       );
