a0fae69ebe77338aefefdd4bc9c76798b7b9453e
RupertDeng <60729982+RupertDeng@users.noreply.github.com>
2022-02-22 23:29:28 -0500

    complete privacy page

--- src/pages/Privacy.js
+++ src/pages/Privacy.js
@@ -1,7 +1,19 @@
 export const Privacy = () => {
   return (
-    <div className='container' style={{minHeight: 'calc(100vh - 125px)'}}>
-      <h2 className='text-white text-center'>Page under construction. Please visit later</h2>
+    <div className='container-lg mt-5' style={{minHeight: 'calc(100vh - 170px)'}}>
+      <div className='w-75 mx-auto text-white'>
+        <h2 className='text-center'>Your privacy is valued here!</h2>
+        <br></br>
+        <br></br>
+        <p><i className='bi bi-layers-fill'></i> &nbsp; This is an anonymous website. Anyone can search phone numbers, post messages, and vote on other's messages without registration or sign-in.</p>
+        <p><i className='bi bi-layers-fill'></i> &nbsp; When leaving messages, please do not disclose any personal information, either yours or others.</p>
+        <p><i className='bi bi-layers-fill'></i> &nbsp; User who posted the message is able to delete it from the website, on the same devive used during posting.</p>
+        <p><i className='bi bi-layers-fill'></i> &nbsp; User will not be able to delete other user's posted message. This is backed by encrypted cookie authentication.</p>
+        <p><i className='bi bi-layers-fill'></i> &nbsp; Brower cookie is used for the sole purpose mentioned above. It will not be used to collect other user information.</p>
+        <p><i className='bi bi-layers-fill'></i> &nbsp; In order to restrict abuse of the message and voting system, visitor's IP is being monitored on the website server during searching/posting/voting actions.</p>
+        <p><i className='bi bi-info-circle-fill'></i> &nbsp; If you feel anything that violates your privacy, please leave detailed message under number (000)000-0000, for administrator to address.</p>
+      </div>
+
     </div>
   );
 };
